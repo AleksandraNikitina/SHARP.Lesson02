36777b638c4f19ab4a8149e0b632cd95610ce788
Aleksandra <asandrabelka@gmail,com>
2024-10-25 03:45:28 +0300

    Change Task2

--- Seminar03/Task2/Program.cs
+++ Seminar03/Task2/Program.cs
@@ -4,5 +4,9 @@ int[] array = new int[10] {-3, 5, 2, 0, -7, 6, 9, 10, 3, -2};
 for (int i = 0; i < array.Length; i++)
 {
     array[i] *= -1; // array[i] = -1 * array[i];
+}
+
+for (int i = 0; i < array.Length; i++)
+{
     Console.Write(array[i] + " ");
 }
