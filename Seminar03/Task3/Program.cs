630fabd694a82ee5cfcc92ba7bae4829f6dc22b6
Aleksandra <asandrabelka@gmail,com>
2024-10-25 04:01:18 +0300

    Add another solution for Task3

--- Seminar03/Task3/Program.cs
+++ Seminar03/Task3/Program.cs
@@ -8,11 +8,26 @@
 int[] array = {1, 3, 0, 2, 7, -5, 4, 2, 3};
 int[] arrResult = new int[array.Length/2];
 
-for (int i = 0; i < array.Length/2; i++)
-{
-    arrResult[i] = array[i] * array[array.Length-1-i];
-}
+// for (int i = 0; i < array.Length/2; i++)
+// {
+//     arrResult[i] = array[i] * array[array.Length-1-i];
+// }
 
+// for (int i = 0; i < arrResult.Length; i++)
+// {
+//     Console.Write(arrResult[i] + " ");
+// }
+
+
+int left = 0;
+int right = array.Length - 1;
+
+while (left < right)
+{
+    arrResult[left] = array[left] * array[right];
+    left++;
+    right--;
+}
 for (int i = 0; i < arrResult.Length; i++)
 {
     Console.Write(arrResult[i] + " ");
