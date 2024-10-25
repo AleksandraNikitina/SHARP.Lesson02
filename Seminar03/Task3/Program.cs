a7c97dd03996cafd2660251bfab1ae82519a0181
Aleksandra <asandrabelka@gmail,com>
2024-10-25 03:47:58 +0300

    Little change program

--- Seminar03/Task3/Program.cs
+++ Seminar03/Task3/Program.cs
@@ -5,7 +5,7 @@
 // [1 3 2 4 2 3] => [3 6 8]
 // [2 3 1 7 5 6 3] => [6 18 5] (7 has no pair)
 
-int[] array = new int[9] {1, 3, 0, 2, 7, -5, 4, 2, 3};
+int[] array = {1, 3, 0, 2, 7, -5, 4, 2, 3};
 int[] arrResult = new int[array.Length/2];
 
 for (int i = 0; i < array.Length/2; i++)
