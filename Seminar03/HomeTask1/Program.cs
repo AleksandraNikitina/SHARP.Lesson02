5a54e743a9199101d01de7232ae49462728a6dd7
Aleksandra <asandrabelka@gmail,com>
2024-10-28 14:48:59 +0200

    Add HomeTask1

---
+++ Seminar03/HomeTask1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задание 1: Поиск элемента в массиве
+
+//  Описание: Задайте массив целых чисел. Напишите программу, которая проверяет,
+//  присутствует ли заданное число в массиве. Программа должна вывести:
+//  Присутствует/Не присутствует.
+
+// Method 1:
+
+// int[] array = {5, 3, 9, 2, 4, 0, -4, 2, -6};
+// int num = 5;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] == num)
+//     {
+//         Console.WriteLine("Exist");
+//         break;
+//     }
+//     else if (i == array.Length-1)
+//     {
+//         Console.WriteLine("Not exist");
+//     }
+// }
+
+
+
+// Method 2:
+
+int[] array = {5, 3, 9, 2, 4, 0, -4, 2, -6};
+int num = 1;
+
+bool isExistNum = false;
+// string result = isExistNum ? "Exist." : "Not exist.";
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break;
+    }    
+}
+Console.WriteLine(isExistNum ? "Exist." : "Not exist.");
