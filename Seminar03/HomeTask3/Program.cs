d9fb6649ca6844f78fb24a209a05233a74fc6c35
Aleksandra <asandrabelka@gmail,com>
2024-10-29 00:23:53 +0200

    Add HomeTask 2 & 3

---
+++ Seminar03/HomeTask3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задание 3: Поиск среднего значения массива
+
+//  Описание: Задайте массив из 10 целых чисел. Найдите среднее значение элементов
+//  массива.
+
+int[] array = new int[10] { 2, -5, 0, 3, 7, -1, 0, -34, -4, 99 };
+int sum = 0;
+
+foreach (int num in array)
+{
+    sum = sum + num;
+}
+int average = sum / array.Length;
+
+Console.WriteLine(average);
