7af07a5321dd69b633dfd640749d7d27cd8dc03b
Aleksandra <asandrabelka@gmail,com>
2024-10-29 01:32:15 +0200

    Add HomeTaskSem2

---
+++ Seminar03/HomeTaskSem2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Program counts even elements of array.
+
+int[] array = new int[10] { 47, 25, 5, 97, 46, 7, 63, 1, 4, 83 };
+int count = 0;
+
+foreach (int num in array)
+{
+    if (num%2==0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
