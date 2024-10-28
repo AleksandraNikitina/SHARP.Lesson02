8604d6914aa83120a00bf7ff5ecae190a14f5200
Aleksandra <asandrabelka@gmail,com>
2024-10-29 01:55:26 +0200

    Add HomeTaskSem3

---
+++ Seminar03/HomeTaskSem3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Create an array of real numbers. 
+// Find a difference between max & min elements of array.
+
+double[] array = new double[] { 4.35, 6.84, 0.28, 1.57, 64.3, 7.35, 25.76 };
+double min = array[0];
+double max = array[0];
+
+foreach (double num in array)
+{
+    if (num > max)
+    {
+        max = num;
+    }
+    else if (num < min)
+    {
+        min = num;
+    }
+}
+
+Console.WriteLine(max-min);
