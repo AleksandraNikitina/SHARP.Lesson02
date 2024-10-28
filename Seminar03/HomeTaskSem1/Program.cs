8f259b08f48719e4b0e494b7aef4c541f82968bb
Aleksandra <asandrabelka@gmail,com>
2024-10-29 01:26:07 +0200

    Add HomeTaskSem1

---
+++ Seminar03/HomeTaskSem1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Program returns array with elements of other array that is in range [10,90].
+
+using System.Globalization;
+
+int[] array = new int[10] { 47, 25, 5, 97, 46, 7, 63, 1, 4, 83 };
+int countElements = 0;
+
+foreach (int num in array)
+{
+    if (num > 10 && num < 90)
+    {
+        countElements++;
+    }
+}
+
+int[] result = new int[countElements];
+int j = 0;
+foreach (int num in array)
+{
+    if (num > 10 && num < 90)
+    {
+        result[j] = num;
+        j++;
+    }
+}
+
+Console.Write("[");
+foreach(int num in result)
+{
+    Console.Write($"{num} ");
+}
+Console.Write("]");
