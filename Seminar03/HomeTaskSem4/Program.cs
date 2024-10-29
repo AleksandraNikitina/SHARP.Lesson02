e94622494d6a126390aaa88afa7d96d3c546b440
Aleksandra <asandrabelka@gmail,com>
2024-10-29 02:28:07 +0200

    Add HomeTaskSem4

---
+++ Seminar03/HomeTaskSem4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Output array of digits of number.
+
+using System.Globalization;
+
+int num = 4;
+int countDigits = 0;
+
+int numCopy = num;
+while (numCopy > 0)
+{
+    countDigits++;
+    numCopy /= 10; // num = num/10;
+}
+// Console.WriteLine(countDigits);
+
+int[] digits = new int[countDigits];
+
+for (int i = countDigits - 1; i >= 0; i--)
+{
+    digits[i] = num % 10;
+    num /= 10;
+}
+
+Console.Write("[");
+foreach (int e in digits)
+{
+    Console.Write($"{e} ");
+}
+Console.Write("]");
